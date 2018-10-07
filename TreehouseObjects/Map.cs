b4e1f429f333459c4975f9f6bf8b7d206b433d11
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-07 17:21:10 -0500

    methods complete

--- TreehouseObjects/Map.cs
+++ TreehouseObjects/Map.cs
@@ -15,5 +15,13 @@ namespace TreehouseObjects
             Width = width;
             Height = height;
         }
+        
+
+        //method
+        public bool OnMap(Point point)
+        {
+            return point.X >= 0 && point.X < Width && 
+                   point.Y >= 0 && point.Y   < Height;
+        }
     }
 }
--- TreehouseObjects/Program.cs
+++ TreehouseObjects/Program.cs
@@ -9,8 +9,10 @@ namespace TreehouseObjects
             Tower tower = new Tower();
             Map map = new Map(8, 5);
 
-            int area = map.Width + map.Height;
+            Point point = new Point(4, 2);
+            Console.WriteLine(point.DistanceTo(5, 5)); 
 
+            Console.Read();
         }
     }
 }
