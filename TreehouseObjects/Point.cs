5f08dc07458cbf142a937335aa0e6032b0df9f51
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-07 16:12:09 -0500

    added unsaved changes

--- TreehouseObjects/Point.cs
+++ TreehouseObjects/Point.cs
@@ -6,5 +6,13 @@ namespace TreehouseObjects
 {
     class Point
     {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
