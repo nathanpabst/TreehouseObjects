53ed3f149cb01ce15377ad18622e234c4da098f3
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-10 19:07:27 -0500

    added MapLocation class inhereting from the Point class

---
+++ TreehouseObjects/MapLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreehouseObjects
+{
+    class MapLocation : Point
+    {
+        public MapLocation(int x, int y) : base(x, y)
+        { }
+    }
+}
--- TreehouseObjects/Program.cs
+++ TreehouseObjects/Program.cs
@@ -4,69 +4,79 @@ namespace TreehouseObjects
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //Tower tower = new Tower();
-            //Map map = new Map(8, 5);
+            Map map = new Map(8, 5);
 
-            //Point point = new Point(4, 2);
-            //Console.WriteLine(point.DistanceTo(5, 5)); 
+            Point x = new MapLocation(4, 2);
 
-            //------------ADDITIONAL PRACTICE WITH METHODS--------------
-            var book1 = new Book("The Fountainhead", "Ayn Rand");
-            Console.WriteLine(book1.GetDisplayText());
+            Point p = x;
 
-            book1.Loan("Natz");
-            Console.WriteLine(book1.GetDisplayText());
-
-            book1.Return();
-            Console.WriteLine(book1.GetDisplayText());
+            map.OnMap(new MapLocation(0, 0));
+            Console.WriteLine(x.DistanceTo(5, 5));
+            Console.WriteLine(x is MapLocation);
+            Console.WriteLine(x is Point);
+            Point point = new Point(0, 0);
+            Console.WriteLine(point is MapLocation);
 
             Console.Read();
         }
+            //        //------------ADDITIONAL PRACTICE WITH METHODS--------------
+            //        var book1 = new Book("The Fountainhead", "Ayn Rand");
+            //        Console.WriteLine(book1.GetDisplayText());
 
-    }
-    class Book
-    {
-        public readonly string Title;
-        public readonly string Author;
-        public string Loanee = null;
-        public bool OnLoan = false;
+            //        book1.Loan("Natz");
+            //        Console.WriteLine(book1.GetDisplayText());
 
-        public Book(string title, string author)
-        {
-            Title = title;
-            Author = author;
+            //        book1.Return();
+            //        Console.WriteLine(book1.GetDisplayText());
+
+            //        Console.Read();
+            //    }
+
+            //}
+            //class Book
+            //{
+            //    public readonly string Title;
+            //    public readonly string Author;
+            //    public string Loanee = null;
+            //    public bool OnLoan = false;
+
+            //    public Book(string title, string author)
+            //    {
+            //        Title = title;
+            //        Author = author;
+            //    }
+
+            //    public void Loan(string loanee)
+            //    {
+            //        Loanee = loanee;
+            //        OnLoan = true;
+            //    }
+
+            //    public void Return()
+            //    {
+            //        Loanee = null;
+            //        OnLoan = false;
+            //    }
+
+            //    public string GetDisplayText()
+            //    {
+            //        string text = "Book: " + Title + " by " + Author;
+
+            //        if (OnLoan)
+            //        {
+            //            text += " (Currently on loan to " + Loanee + ")";
+            //            return text;
+
+            //        }
+            //        else
+            //        {
+            //            return text;
+            //        }
+            //    }
+
         }
-
-        public void Loan(string loanee)
-        {
-            Loanee = loanee;
-            OnLoan = true;
-        }
-
-        public void Return()
-        {
-            Loanee = null;
-            OnLoan = false;
-        }
-
-        public string GetDisplayText()
-        {
-            string text = "Book: " + Title + " by " + Author;
-
-            if (OnLoan)
-            {
-                text += " (Currently on loan to " + Loanee + ")";
-                return text;
-
-            }
-            else
-            {
-                return text;
-            }
-        }
-
-    }
     
 }
