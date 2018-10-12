f669f2c076848d200471cb3b2ec5dda9792540be
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-11 20:28:49 -0500

    added fields, a constructor, and a method to the invader class

--- TreehouseObjects/Invader.cs
+++ TreehouseObjects/Invader.cs
@@ -6,8 +6,28 @@ namespace TreehouseObjects
 {
     class Invader
     {
+        //field to store an instance of the Path object allowing us to update the location and access the info later. call _path in the Invader constructor
+        private readonly Path _path;
+
         //using auto-property. we can remove the private MapLocation _location line because private is the default value
         public MapLocation Location { get; private set; }
+
+        //using _pathStep field, initialized at 0, to keep track of where the invader is along the path
+        private int _pathStep = 0;
+
+        //constructor method to set the location on the path
+        public Invader(Path path)
+        {
+            _path = path; 
+            Location = _path.GetLocationAt(_pathStep);
+        }
+
+        //method to advance invader down the path. public because it needs to be accessable by other classes.
+        public void Move()
+        {
+            _pathStep += 1;
+            Location = _path.GetLocationAt(_pathStep);
+        }
 
         //MapLocation needs to be public access modifier so other classes can access the location. We can't use the readonly access modifier because the location will change throughout the game. but public exposes too much of the internal workings of the class--poor encapsulation. Instead we should make the location field private and then write public methods to access the field.
         //_location makes this a private field. The following line is only storing information.
