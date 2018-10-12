327c17ccbac684797b1a7b6a6e3679c17ebb4306
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-12 18:13:52 -0500

    refactored code using syntactic sugar to reduce the lines of code and added more properties to determine health and score of the invader.

--- TreehouseObjects/Path.cs
+++ TreehouseObjects/Path.cs
@@ -7,6 +7,9 @@ namespace TreehouseObjects
     class Path
     {
         private readonly MapLocation[] _path;
+
+        public int Length => _path.Length;
+
         public Path(MapLocation[] path)
         {
             _path = path;
