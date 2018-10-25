b3281c5f31070823e1c4ab18d9c79a4ff6b69d3c
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-24 20:03:23 -0500

    added powerful tower class to the game

---
+++ TreehouseObjects/PowerfulTower.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreehouseObjects
+{
+    class PowerfulTower : Tower
+    {
+        protected override int Power { get; } = 2;
+        public PowerfulTower(MapLocation location) : base(location)
+        {
+
+        }
+    }
+}
--- TreehouseObjects/Program.cs
+++ TreehouseObjects/Program.cs
@@ -34,8 +34,8 @@ namespace TreehouseObjects
 
                 Tower[] towers =
                 {
-                    new Tower(new MapLocation(1,3,map)),
-                    new Tower(new MapLocation(3,3,map)),
+                    new SniperTower(new MapLocation(1,3,map)),
+                    new PowerfulTower(new MapLocation(3,3,map)),
                     new Tower(new MapLocation(3,4,map))
                 };
 
