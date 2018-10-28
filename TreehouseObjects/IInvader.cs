c3048739aec255568c76c935112ba14ed6d773d8
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-28 15:03:32 -0500

    added IMoveable and IMappable interfaces

--- TreehouseObjects/IInvader.cs
+++ TreehouseObjects/IInvader.cs
@@ -4,12 +4,22 @@ using System.Text;
 
 namespace TreehouseObjects
 {
+    //interfaces can only declare the public members that subclasses must have. the point is to only expose what is needed by the code that is using the class
+    //classes can only inherit directly from one other class. interface solves for multiple inheritance. a class can implement as many interfaces as needed.
+    interface IMappable
+    {
+        MapLocation Location { get; }
+    }
+
+    interface IMovable
+    {
+        void Move();
+
+    }
     //the interface defines what the properties and methods are and what their parameter and return types are. 
     //methods and properties in an Interface do not have implementation. Interfaces only define public members..so we can delete that term for each. virtual and abstract modifiers are also not needed in Interfaces.
-    interface IInvader
+    interface IInvader : IMappable, IMovable
     {
-
-        MapLocation Location { get; }
 
         bool HasScored { get; }
 
@@ -19,10 +29,7 @@ namespace TreehouseObjects
 
         bool IsActive { get; }
 
-        void Move();
-
         void DecreaseHealth(int factor);
-
 
     }
 }
