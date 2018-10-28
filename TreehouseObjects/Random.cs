5caf8389562b3ef6aaa3c2bf0df935a831c3cd99
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-28 17:18:33 -0500

    wip

---
+++ TreehouseObjects/Random.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreehouseObjects
+{
+    //using System.Random to create a global Random object. This will simplify our code in Tower and ShieldedInvader
+    //static classes can't inherit from other classes or be inherited from
+    static class Random
+    {
+            private static System.Random _random = new System.Random();
+
+            public static double NextDouble()
+            {
+                return _random.NextDouble();
+            }
+        
+    }
+}
